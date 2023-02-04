b5eda4e0484f0e19cb6a300ea6f38452c94e0d91
Defelo <elodef42@gmail.com>
2023-02-04 17:55:36 +0100

    Add companies migration

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -2,9 +2,11 @@ pub use sea_orm_migration::prelude::*;
 
 pub struct Migrator;
 
+mod m20230204_171617_create_companies_table;
+
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![]
+        vec![Box::new(m20230204_171617_create_companies_table::Migration)]
     }
 }
