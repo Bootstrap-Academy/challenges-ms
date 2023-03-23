95777b42b33a43e5bbdff6e2047784607d6a5ec7
Defelo <elodef42@gmail.com>
2023-03-23 08:16:47 +0100

    Generate entities for challenges

--- entity/src/challenges_skill_tasks.rs
+++ entity/src/challenges_skill_tasks.rs
@@ -3,30 +3,28 @@
 use sea_orm::entity::prelude::*;
 
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq)]
-#[sea_orm(table_name = "jobs_skill_requirements")]
+#[sea_orm(table_name = "challenges_skill_tasks")]
 pub struct Model {
     #[sea_orm(primary_key, auto_increment = false)]
-    pub job_id: Uuid,
-    #[sea_orm(primary_key, auto_increment = false, column_type = "Text")]
-    pub skill_id: String,
-    pub level: i32,
+    pub task_id: Uuid,
+    pub skill_id: Uuid,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
 pub enum Relation {
     #[sea_orm(
-        belongs_to = "super::jobs_jobs::Entity",
-        from = "Column::JobId",
-        to = "super::jobs_jobs::Column::Id",
+        belongs_to = "super::challenges_tasks::Entity",
+        from = "Column::TaskId",
+        to = "super::challenges_tasks::Column::Id",
         on_update = "NoAction",
         on_delete = "Cascade"
     )]
-    JobsJobs,
+    ChallengesTasks,
 }
 
-impl Related<super::jobs_jobs::Entity> for Entity {
+impl Related<super::challenges_tasks::Entity> for Entity {
     fn to() -> RelationDef {
-        Relation::JobsJobs.def()
+        Relation::ChallengesTasks.def()
     }
 }
 
