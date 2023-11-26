f3f8b86e094c42783bfdd58d912c7b99c9e5bf38
Defelo <elodef42@gmail.com>
2023-11-26 23:25:30 +0100

    Add NixOS module

--- flake.nix
+++ flake.nix
@@ -1,12 +1,11 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    fenix.url = "github:nix-community/fenix";
   };
 
   outputs = {
+    self,
     nixpkgs,
-    fenix,
     ...
   }: let
     defaultSystems = [
@@ -22,14 +21,74 @@
         })
         defaultSystems);
   in {
+    packages = eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      default = self.packages.${system}.challenges;
+      challenges = pkgs.rustPlatform.buildRustPackage {
+        inherit ((fromTOML (builtins.readFile ./challenges/Cargo.toml)).package) version;
+        pname = "academy-challenges";
+        src = ./.;
+        cargoLock.lockFile = ./Cargo.lock;
+        doCheck = false;
+      };
+    });
+    nixosModules.default = {
+      config,
+      lib,
+      pkgs,
+      ...
+    }: let
+      settingsFormat = pkgs.formats.toml {};
+    in {
+      options.academy.backend.challenges = with lib; {
+        enable = mkEnableOption "Bootstrap Academy Challenges Microservice";
+        RUST_LOG = mkOption {
+          type = types.str;
+          default = "info";
+        };
+        environmentFiles = mkOption {
+          type = types.listOf types.path;
+        };
+        settings = mkOption {
+          inherit (settingsFormat) type;
+        };
+      };
+
+      config = let
+        cfg = config.academy.backend.challenges;
+      in
+        lib.mkIf cfg.enable {
+          systemd.services = {
+            academy-challenges = {
+              wantedBy = ["multi-user.target"];
+              serviceConfig = {
+                User = "academy-challenges";
+                Group = "academy-challenges";
+                DynamicUser = true;
+                EnvironmentFile = cfg.environmentFiles;
+              };
+              environment = {
+                inherit (cfg) RUST_LOG;
+                CONFIG_PATH = settingsFormat.generate "config.toml" cfg.settings;
+              };
+              preStart = ''
+                ${self.packages.${pkgs.system}.default}/bin/migration
+              '';
+              script = ''
+                ${self.packages.${pkgs.system}.default}/bin/challenges
+              '';
+            };
+          };
+        };
+    };
     devShells = eachDefaultSystem (system: let
       inherit (nixpkgs) lib;
       pkgs = import nixpkgs {inherit system;};
-      rust = fenix.packages.${system}.stable.toolchain;
       devShell = withRust:
         pkgs.mkShell {
           packages = with pkgs;
-            lib.optional withRust rust
+            lib.optionals withRust [rustc cargo clippy rust-analyzer]
             ++ [
               just
               postgresql
